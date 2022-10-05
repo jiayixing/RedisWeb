fa4572393748cf94c5187d66c2799edfd55610c2
Administrator <1102242300@qq.com>
2022-10-05 13:03:25 +0800

    第一次提交

--- src/main/java/com/xingxingjia/redisweb/controller/RedisController.java
+++ src/main/java/com/xingxingjia/redisweb/controller/RedisController.java
@@ -4,6 +4,8 @@ public class RedisController {
 
     public static void main(String[] args) {
         System.out.println("woaini");
+        System.out.println("woaini");
+        System.out.println("woaini");
     }
 }
 
