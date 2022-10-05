2f567fb6acb2102832bea5b71578bca5824300ac
Administrator <1102242300@qq.com>
2022-10-05 13:00:00 +0800

    第一次提交

---
+++ src/main/java/com/xingxingjia/redisweb/RedisWebApplication.java
@@ -0,0 +1,13 @@
+package com.xingxingjia.redisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisWebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xingxingjia/redisweb/controller/RedisController.java
@@ -0,0 +1,9 @@
+package com.xingxingjia.redisweb.controller;
+
+public class RedisController {
+
+    public static void main(String[] args) {
+        System.out.println("woaini");
+    }
+}
+
